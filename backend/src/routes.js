16b9119950a1c7b2cafe14d604e9bfba20643f78
lopeswestphal <lopeswestphal@hotmail.com>
2020-03-27 12:00:49 -0300

    Front-end do app

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -20,30 +20,3 @@ routes.delete('/incidents/:id', incidentController.delete);
 
 // Exportando uma variavel de um arquivo
 module.exports = routes; 
-
-/*
- > Rota: e o caminho completo / Recurso: e onde vc quer chegar
-*/ 
-
-/*
-> Metodos HTTP:
->
-> GET: Buscar/listas uma informação do back-end
-> POST: Criar uma informção no back-end
-> PUT: Alterar uma informação no back-end
-> DELETE: Deletar  = = = = = = = = == == =
-*/
-
-/*
-> Tipos De Parâmetros:
->
-> Query Params: Parâmetros nomeados enviados na rota apos "?" (Filtros, Paginação ...)
-> Route Params: Parâmetros utilizados para indentificar recursos "/:id" ".params"
-> Request Body: Corpo da requisisão, utilizado para criar ou alterar recursos 
-*/
-
-/** 
- * SQL: MySQL, Oracle, Microsoft SQL Sever, SQLlite.
- * NoSQL: MongoDB, etc..
- * Comunicação: 1.Driver -> Select * FROM users / 2.Query Builder -> table('users').select('*').where() 
-*/
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+/*
+* Um componete no react e uma função que retorna HTML(quando esta dentro de um aqrquivo JS ele e
+* chamado de "JSX -> JavaScript XML")
+*/
+// Ele esta renderizando(colocando na tela)
+ReactDOM.render( <React.StrictMode> <App /></React.StrictMode>,document.getElementById('root'));
